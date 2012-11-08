a81aaf5d208c817244086bb16437e84bb7d0bdd9
Andrew Carlberg <aec1kt@gmail.com>
2012-11-08 13:46:54 -0600

    Reviewer grabbing code fixed

--- beeradvocatecrawlerreviews.py
+++ beeradvocatecrawlerreviews.py
@@ -19,28 +19,14 @@ def remove_extra_spaces(data):
 
 def find_list_of_beers(data,json,beerId):
     ratingPattern = re.compile(r'<span class=\"BAscore_norm\">\d+\.\d*</span>')
-    reviewPattern = re.compile(r'<b>\w+</b>')
+    reviewPattern = re.compile(r'<b>\w+</b></a></h6>')
     reviewers = re.findall(reviewPattern,data)
     ratings = re.findall(ratingPattern, data)
-    reviewers.remove('<b>Beers</b>')
-    reviewers.remove('<b>Events</b>')
-    try:
-        reviewers.remove('<b>Reviews</b>')
-        reviewers.remove('<b>average</b>')
-        reviewers.remove('<b>average</b>')
-        reviewers.remove('<b>world-class</b>')
-        reviewers.remove('<b>exceptional</b>')
-        reviewers.remove('<b>good</b>')
-        reviewers.remove('<b>poor</b>')
-        reviewers.remove('<b>avoid/b>')
-        reviewers.remove('<b>must try</b>')
-        reviewers.remove('<b>try it</b>')
-    except:
-        pass
+    print reviewers
     for x in ratings:
         rating = x.replace('<span class=\"BAscore_norm\">','')
         rating = rating.replace('</span>','')
-        reviewer = reviewers[ratings.index(x)].replace('<b>','').replace('</b>','')
+        reviewer = reviewers[ratings.index(x)].replace('<b>','').replace('</b></a></h6>','')
         beerDump = {"BeerId":beerId, "Reviewer":reviewer,"rating":rating}
         s = ujson.dumps(beerDump)
         json.write(s+'\n')
